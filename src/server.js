16df0f3de051c186618aa5c681523d3969eda725
Thiago <bussoladev@gmail.com>
2024-02-04 20:13:49 -0300

    feat(*): config status code and methods

--- src/server.js
+++ src/server.js
@@ -1,17 +1,29 @@
 import http from 'node:http';
 
+
+const users = [];
+
 const server = http.createServer((req, res) => {
-    const {method , url} = req;
+    const { method, url } = req;
 
-    if(method === 'GET' && url === '/users') {
-        return res.end('Listagem de usuários')
+    if (method === 'GET' && url === '/users') {
+        return res
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users))
     }
 
-    if(method === 'POST' && url === '/users') {
-        return res.end('Criação de usuários')
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@example.com'
+        })
+        return res.writeHead(201).end()
     }
 
-    return res.end('Hello World!')
+    return res
+        .writeHead(404)
+        .end('Not found')
 })
 
 server.listen(3333)
