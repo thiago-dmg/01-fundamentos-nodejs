83d14e0903f9a1ebc1398272b1474544cc841ecd
Thiago <bussoladev@gmail.com>
2024-02-05 16:10:15 -0300

    feat(*) streams

---
+++ streams/fundamentals.js
@@ -0,0 +1,35 @@
+import { Readable, Transform, Writable} from "node:stream";
+
+class OneHundredStream extends Readable {
+    index = 1;
+    _read() {
+        const i = this.index++;
+
+     setTimeout(() => {
+        if(i > 100) {
+            this.push(null)
+        } else {
+            const buf = Buffer.from(String(i))
+            this.push(buf)
+        }
+     }, 1000)
+    }
+}
+
+class InverseNumberStream extends Transform {
+    _transform(chunk, encode , callback) {
+        const transformed = Number(chunk.toString()) * - 1
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+class MultiplyByTenStream extends Writable {
+    _write(chunk, encode , callback) {
+        console.log(Number(chunk.toString()) * 10)
+        callback()
+    }
+}
+
+new OneHundredStream()
+    .pipe(new InverseNumberStream())
+    .pipe(new MultiplyByTenStream())
